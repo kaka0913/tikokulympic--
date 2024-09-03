8c01e92c7cb75b9f3d4b3bf865b5c8885c88398c
kaka0913 <kabuyuu0913@gmail.com>
2024-09-03 23:55:43 +0900

    Initial Commit

---
+++ TikokulympicBeta/TikokulympicBetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikokulympicBetaApp.swift
+//  TikokulympicBeta
+//
+//  Created by 株丹優一郎 on 2024/09/03.
+//
+
+import SwiftUI
+
+@main
+struct TikokulympicBetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
