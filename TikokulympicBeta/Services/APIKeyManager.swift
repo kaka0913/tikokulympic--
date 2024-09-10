774e534deec620462f098ffe1a0331978f609229
kaka0913 <kabuyuu0913@gmail.com>
2024-09-10 18:35:22 +0900

    APIkeyマネージャの導入

---
+++ TikokulympicBeta/Services/APIKeyManager.swift
@@ -0,0 +1,22 @@
+//
+//  APIKeyManager.swift
+//  TikokulympicBeta
+//
+//  Created by 株丹優一郎 on 2024/09/08.
+//
+
+import Foundation
+
+class APIKeyManager {
+    static let shared = APIKeyManager()
+    
+    private init() {}
+    
+    func apiKey(for service: String) -> String? {
+        guard let keys = Bundle.main.infoDictionary?["APIKeys"] as? [String: Any],
+              let key = keys[service] as? String else {
+            return nil
+        }
+        return key
+    }
+}
