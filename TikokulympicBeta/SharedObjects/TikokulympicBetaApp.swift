58eda1f4100184b745059088e5ff2781ef74fd99
kaka0913 <kabuyuu0913@gmail.com>
2024-09-15 13:08:34 +0900

    通知の許可等の処理を追加

--- TikokulympicBeta/SharedObjects/TikokulympicBetaApp.swift
+++ TikokulympicBeta/SharedObjects/TikokulympicBetaApp.swift
@@ -4,18 +4,98 @@
 //
 //  Created by 株丹優一郎 on 2024/09/03.
 //
-
-import SwiftUI
-import FirebaseCore
 import GoogleSignIn
+import SwiftUI
+import Firebase
+import FirebaseCore
+import FirebaseMessaging
+import UserNotifications
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
 
-    return true
-  }
+        Messaging.messaging().delegate = self
+
+        UNUserNotificationCenter.current().delegate = self
+
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+
+        application.registerForRemoteNotifications()
+        
+        // 通知の許可をリクエスト
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions
+        ) { granted, error in
+            // 許可が得られたか、エラーが発生したかを処理
+            if let error = error {
+                print("通知の許可リクエストでエラー: \(error.localizedDescription)")
+            } else if granted {
+                print("通知が許可されました")
+            } else {
+                print("通知が拒否されました")
+            }
+        }
+
+        Messaging.messaging().token { token, error in
+            if let error {
+                print("Error fetching FCM registration token: \(error)")
+            } else if let token {
+                print("FCM registration token: \(token)")
+            }
+        }
+
+        return true
+    }
+
+    func application(_: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register for remote notifications with error \(error)")
+    }
+
+    //TODOいずれ消す
+    func application(_: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        var readableToken = ""
+        for index in 0 ..< deviceToken.count {
+            readableToken += String(format: "%02.2hhx", deviceToken[index] as CVarArg)
+        }
+        print("Received an APNs device token: \(readableToken)")
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    @objc func messaging(_: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("Firebase token: \(String(describing: fcmToken))")
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(
+        _: UNUserNotificationCenter,
+        willPresent _: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([.banner, .list, .sound])
+    }
+
+    //ユーザーが通知をタップしたとき、またはカスタムアクションを実行したときに呼び出される
+    //通知に含まれる情報を取り出し、アプリ内で適切な処理を行うために他の部分に伝達する
+    func userNotificationCenter(
+        _: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
+        //アプリ内の他の部分に対して、通知を受信したことを知らせる
+        let userInfo = response.notification.request.content.userInfo
+        NotificationCenter.default.post(
+            name: Notification.Name("didReceiveRemoteNotification"),
+            object: nil,
+            userInfo: userInfo
+        )
+        completionHandler()
+    }
 }
 
 @main
